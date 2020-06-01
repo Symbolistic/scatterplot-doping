83f303fd7830a005ffe8b26719fc9273cf22edc3
Symbol <Symbolistic@live.com>
2020-06-01 06:37:52 -0400

    Updates

---
+++ precache-manifest.a2082d9ea0fc5cb3d017475511ea8194.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c9ef0ed050294159616df750fd985c9",
+    "url": "/scatterplot-doping/index.html"
+  },
+  {
+    "revision": "5561509fcf9df1b169a9",
+    "url": "/scatterplot-doping/static/css/main.5080e53e.chunk.css"
+  },
+  {
+    "revision": "3e461c4d2aabb14ce3ed",
+    "url": "/scatterplot-doping/static/js/2.593ffaec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scatterplot-doping/static/js/2.593ffaec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5561509fcf9df1b169a9",
+    "url": "/scatterplot-doping/static/js/main.7617d34a.chunk.js"
+  },
+  {
+    "revision": "10d2bf581a7892e172f9",
+    "url": "/scatterplot-doping/static/js/runtime-main.bc1ee622.js"
+  }
+]);
